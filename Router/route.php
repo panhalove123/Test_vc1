51dedb27931396ca009718c761fd0d203f7382a5
kimchhik-chhuong <kimchhik.chhuong@gmail.com>
2025-02-26 10:57:22 +0700

    about

--- Router/route.php
+++ Router/route.php
@@ -3,9 +3,11 @@ require_once "Router.php";
 require_once "Controllers/BaseController.php";
 require_once "Database/Database.php";
 require_once "Controllers/WelcomeController.php";
+require_once "Controllers/AboutController.php";
 
 
 $route = new Router();
 $route->get("/", [WelcomeController::class, 'welcome']);
+$route->get("/", [AboutController::class, 'about']);
 
 $route->route();
---
+++ views/about/about.php
@@ -0,0 +1,4 @@
+<div class="container">
+    <h1>Hello about!</h1>
+</div>
+<?php 
