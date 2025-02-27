b948aa1570bb32a4db33651e9eb5380db23dd93b
darinhoy <darin.hoy@student.passerellesnumeriques.org>
2025-02-27 09:41:16 +0700

    add register

--- Router/route.php
+++ Router/route.php
@@ -4,11 +4,14 @@ require_once "Controllers/BaseController.php";
 require_once "Database/Database.php";
 require_once "Controllers/WelcomeController.php";
 require_once "Controllers/RegisterController.php";
+require_once "Controllers/LoginController.php";
 
 
 $route = new Router();
 $route->get("/", [WelcomeController::class, 'welcome']);
 
-$route->get("/register", [ResgisterControll::class, 'register']);
+$route->get("/register", [ResgisterController::class, 'register']);
+
+$route->get("/login", [LoginController::class, 'index']);
 
 $route->route();
